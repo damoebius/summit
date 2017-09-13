aa765b9dac8a6ce25f25e673fd9264a60f595c95
damoebius <tintin33$>
2017-09-13 16:12:23 +0200

    photo item

--- src/components/GalleryView.hx
+++ src/components/GalleryView.hx
@@ -12,14 +12,14 @@ class GalleryView extends HTMLComponent {
     @skinpart("") private var _titleDiv:DivElement;
     @skinpart("") private var _contentDiv:DivElement;
 
-    private function new() {
+    private function new( ) {
         super();
     }
 
-    public function displayAlbum(album:Album):Void{
+    public function displayAlbum( album:Album ):Void {
         _titleDiv.innerHTML = album.Name;
         _contentDiv.innerHTML = "";
-        for(picture in album.Content){
+        for ( picture in album.Content ) {
             var item:PhotoItem = HTMLApplication.createInstance(PhotoItem);
             item.init(picture);
             _contentDiv.appendChild(item);
