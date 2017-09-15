abc55c57c66e1ec911987cad0e4ced7f41a440bf
Thomas Vermot-Gauchy <t.vermot-gauchy@happy-technologies.com>
2017-09-15 11:33:31 +0200

    add style to the gallery

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 http://api.heidi.tech/Api.svc/GetAlbums
 {"Locale":"fr_FR","Token":"F7CF4DD5-ECBF-4CD6-9E7D-29C513C17401"}
+
+You can compile sass file by executing `compass compile` from the project root folder
--- src/items/PhotoItem.hx
+++ src/items/PhotoItem.hx
@@ -1,13 +1,16 @@
 package items;
 
 import model.Picture;
+import js.html.DivElement;
 import js.html.ImageElement;
 import org.tamina.html.component.HTMLComponent;
+import org.tamina.events.html.MouseEventType;
 
 @view("items/PhotoItem.html")
 class PhotoItem extends HTMLComponent{
 
     @skinpart("") var _photoImage:ImageElement;
+    @skinpart("") var _container:DivElement;
 
     private function new(){
         super();
@@ -15,6 +18,17 @@ class PhotoItem extends HTMLComponent{
 
     public function init(picture:Picture){
         _photoImage.src = picture.Url;
+
+        _container.addEventListener(MouseEventType.CLICK, clickHandler);
     }
 
+    private function clickHandler(){
+    	if (_container.classList.contains('opened')) {
+    		trace('opened');
+    		_container.classList.remove('opened');
+    	} else {
+    		trace('not opened');
+    		_container.classList.add('opened');
+    	}
+    }
 }
